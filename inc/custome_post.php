f97d1ce84aa67fbaa0e929eb6bf54cb1a12996cf
Md Shakibur Rahman <mdshakiburrahman6@gmail.com>
2025-12-03 16:54:58 +0600

    created a new custome field for portfolio

--- inc/custome_post.php
+++ inc/custome_post.php
@@ -23,7 +23,42 @@ function custome_services(){
         'show_ui' => true,
         'capability_type' => 'post',
         'rewrite' => array('slug' => 'services'),
-        'supports' => array('title', 'thumbnail', 'editor'),
+        'supports' => array('title', 'thumbnail', 'editor', 'excerpt'),
     ) );
 }
 add_action('init','custome_services');  
+
+
+
+
+// Custome Post Portfolios
+function custome_post_portfolio(){
+
+    register_post_type('portfolio', array(
+
+        'labels' => array(
+            'name' => __('Portfolio', 'my_theme_07'),
+            'singular_name' => __('All Portfolios', 'my_theme_07'),
+            'add_new' => __('Add New Portfolio', 'my_theme_07'),
+            'add_new_item' => __('Add New Portfolio', 'my_theme_07'),
+            'edit_item' => __('Edit Portfolio', 'my_theme_07'),
+            'new_item' => __('New Portfolio', 'my_theme_07'),
+            'view_item' => __('View Portfolio', 'my_theme_07'),
+            'not_found' => __('No porfolio found! Add new portfolio', 'my_theme_07'),
+        ),
+        'menu_icon' => 'dashicons-portfolio',
+        'menu_position' => 6,
+        'public' => true,
+        'publicly_queryable' => true,
+        'exclude_from_search' => true,
+        'has_archive' => true,
+        'hierarchical' => true,
+        'show_ui' => true,
+        'capability_type' => 'post',
+        'rewrite' => array('slug' => 'portfiolo'),
+        'supports' => array('title', 'thumbnail', 'editor', 'excerpt', 'page-attributes'),
+    ));
+
+}
+add_action('init', 'custome_post_portfolio');
+
